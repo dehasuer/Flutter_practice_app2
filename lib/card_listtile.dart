8e5587674d7f2d850385d8a7c24947ba47f8c602
Deha Efecan Süer <dehasuer97@gmail.com>
2023-02-23 17:33:09 +0300

    project added

---
+++ lib/card_listtile.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class CardveListtileKullanimi extends StatelessWidget {
+  const CardveListtileKullanimi({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Cardlisttile Kullanımı"),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            tekListeElemani(),
+            tekListeElemani(),
+            tekListeElemani(),
+            tekListeElemani(),
+            tekListeElemani(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Column tekListeElemani() {
+    return Column(
+      children: [
+        Card(
+          color: Colors.blue.shade100,
+          shadowColor: Colors.red,
+          elevation: 3,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+          child: ListTile(
+            leading: CircleAvatar(
+              child: Icon(Icons.add),
+            ),
+            title: Text("Başlık kısmı"),
+            subtitle: Text("Subtitle kısmı"),
+            trailing: Icon(Icons.real_estate_agent),
+          ),
+        ),
+        Divider(
+          color: Colors.red,
+          thickness: 1,
+          height: 10,
+          indent: 60,
+          endIndent: 60,
+        ),
+      ],
+    );
+  }
+}
