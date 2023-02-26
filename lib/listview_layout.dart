4dec73f87e74fa430e1c107c53dab616ff9f4f31
Deha Efecan Süer <dehasuer97@gmail.com>
2023-02-26 17:01:10 +0300

    .

--- lib/listview_layout.dart
+++ lib/listview_layout.dart
@@ -10,23 +10,58 @@ class ListviewLayout extends StatelessWidget {
         title: Text("Layout Issues"),
       ),
       body: Container(
+        height: 200,
         child: ListView(
-          shrinkWrap:
-              true, //sadece içindeki çocukların boyutları kadar yer kaplar
+          scrollDirection: Axis.horizontal,
+          //shrinkWrap:
+          //     true, //sadece içindeki çocukların boyutları kadar yer kaplar
           children: [
             Container(
-              height: 200,
+              width: 200,
               color: Colors.orange.shade200,
             ),
             Container(
-              height: 200,
+              width: 200,
               color: Colors.orange.shade100,
+            ),
+            Container(
+              width: 200,
+              color: Colors.orange.shade300,
+            ),
+            Container(
+              width: 200,
+              color: Colors.orange.shade500,
             ),
           ],
         ),
         decoration:
             BoxDecoration(border: Border.all(width: 4, color: Colors.red)),
       ),
+    );
+  }
+
+  Column columnIcindeListe() {
+    return Column(
+      children: [
+        Text("Başladı"),
+        Expanded(
+          child: ListView(
+            //shrinkWrap:
+            //     true, //sadece içindeki çocukların boyutları kadar yer kaplar
+            children: [
+              Container(
+                height: 200,
+                color: Colors.orange.shade200,
+              ),
+              Container(
+                height: 200,
+                color: Colors.orange.shade100,
+              ),
+            ],
+          ),
+        ),
+        Text("Bitti")
+      ],
     );
   }
 }
