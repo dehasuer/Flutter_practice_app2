e9fdb9e1cc2fed07f2c90d8bec9ffd099426d7cb
Deha Efecan Süer <dehasuer97@gmail.com>
2023-03-02 00:42:47 +0300

    .

---
+++ lib/silverlist.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SilverList extends StatefulWidget {
+  const SilverList({super.key});
+
+  @override
+  State<SilverList> createState() => _SilverListState();
+}
+
+class _SilverListState extends State<SilverList> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
