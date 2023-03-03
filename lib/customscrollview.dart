f929b0c35aab9eaab62ea16a24c75e7a3dc58deb
Deha Efecan Süer <dehasuer97@gmail.com>
2023-03-03 23:37:38 +0300

    .

---
+++ lib/customscrollview.dart
@@ -0,0 +1,13 @@
+class CustomScrollView extends StatefulWidget {
+  const CustomScrollView({super.key});
+
+  @override
+  State<CustomScrollView> createState() => _CustomScrollViewState();
+}
+
+class _CustomScrollViewState extends State<CustomScrollView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
